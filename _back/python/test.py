ea756b15623bf492c91d0053d258736c6d2eafd1
alex <zaqepa@gmail.com>
2015-10-11 18:21:28 +0300

    redis soset

--- _back/python/test.py
+++ _back/python/test.py
@@ -1,6 +1,6 @@
 __author__ = 'alex'
 from world import World, Body, Polygon
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import math
 import numpy as np
 from flask import Flask
@@ -12,19 +12,49 @@ import threading
 import gevent
 
 
-i = 0
+# i = 0
+#
+# def w(a):
+#     print a
+#
+# def qwerty():
+#     global i
+#     while True:
+#         gevent.spawn(w, 'qwerty' + str(i))
+#         i += 1
+#         gevent.sleep(0.02)
+#
+# gevent.spawn(qwerty)
+# print '1'
+# gevent.sleep(5.0)
 
-def w(a):
-    print a
+import redislite
+import json
+#
+r = redislite.StrictRedis()
+m = {}
+ar = 1234567890.0
 
 
-def qwerty():
-    global i
-    while True:
-        gevent.spawn(w, 'qwerty' + str(i))
-        i += 1
-        gevent.sleep(0.02)
+print 'redis'
+start = time.time()
 
-gevent.spawn(qwerty)
-print '1'
-gevent.sleep(5.0)
+for i in range(10000):
+    r.set(str(i), ar + i)
+for i in range(10000):
+    ar2 = r.get(str(i))
+    # ar2 *= 2
+
+print time.time() - start
+
+
+print 'dict'
+start = time.time()
+
+for i in range(10000):
+    m[str(i)] = ar + i
+for i in range(10000):
+    ar2 = m.get(str(i))
+    # ar2 *= 2
+
+print time.time() - start
